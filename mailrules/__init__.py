0b80a9028a365c41ca9f7cc90937f22a07d95b9d
Dara Poon <dwpoon@mail.ubc.ca>
2021-05-17 16:12:02 -0700

    setuptools packaging

--- mailrules/__init__.py
+++
@@ -1,2 +0,0 @@
-class UnresolvedLocalEmailAddressException(Exception):
-    pass
--- src/mailrules/cli.py
+++ src/mailrules/cli.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 
 # Copyright 2020-2021 Dara Poon and the University of British Columbia
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import argparse
 from collections import namedtuple, OrderedDict
@@ -13,6 +26,7 @@ try:
     from mailrules.custom import post_process
 except ImportError:
     post_process = lambda script, conversion_context: script
+import mailrules
 import mailrules.forward_to_sieve
 import mailrules.procmailrc
 import mailrules.proc_to_sieve
@@ -22,11 +36,16 @@ def parse_args(args=None):
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('-u', '--user',
         type=str,
-        help='username whose configuration to convert',
+        help="username whose configuration to convert",
+    )
+    parser.add_argument('-i', '--inbox',
+        type=str,
+        help="path to the user's inbox, relative to user's home directory"
+             " (defaults to /var/mail/$USER)",
     )
     parser.add_argument('-d', '--domain',
         type=str,
-        help='email domain of the user',
+        help="email domain (for resolving unqualified local addresses)",
     )
     parser.add_argument('-p', '--provenance-comments',
         action='store_true',
@@ -46,7 +65,10 @@ def conversion_context(opts):
             'LOGNAME': opts.user or os.getenv('LOGNAME'),
             'HOME': lambda c: c.directory,
             'MAILDIR': lambda c: c.directory,
-            'DEFAULT': lambda c: c.resolve_path('Maildir'),
+            'DEFAULT': lambda c: os.path.abspath(c.resolve_path(
+                opts.inbox or
+                os.path.join('/var/mail', opts.user or os.getenv('LOGNAME'))
+            )),
             'ORGMAIL': os.path.join('/var/mail', opts.user or os.getenv('LOGNAME')),
             'PATH': '/usr/local/bin:/usr/bin:/bin',
         },
@@ -96,14 +118,18 @@ def sieve_commands(conversion_context):
 def main():
     conv = conversion_context(parse_args())
     out = mailrules.sieve.Script()
-    for cmd in sieve_commands(conv):
-        out.add_command(cmd)
-    out = post_process(out, conv)
-    sieve_script_text = str(out)
-    if sieve_script_text:
-        print(sieve_script_text, end='\r\n')
+    try:
+        for cmd in sieve_commands(conv):
+            out.add_command(cmd)
+        out = post_process(out, conv)
+        sieve_script_text = str(out)
+        if sieve_script_text:
+            print(sieve_script_text, end='\r\n')
 
-    sys.exit(1 if mailrules.proc_to_sieve.FIXME.instances else 0)
+        sys.exit(1 if mailrules.proc_to_sieve.FIXME.instances else 0)
+    except mailrules.UnresolvedLocalEmailAddressException:
+        error("error: the following arguments are required: -d/--domain")
+
 
 if __name__ == '__main__':
     main()
