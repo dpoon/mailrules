96fbf17937415176dd9ce7615442e042492dd2c0
Dara Poon <dwpoon@mail.ubc.ca>
2021-05-26 13:38:22 -0700

    Version 0.2.0

--- src/mailrules/__init__.py
+++ src/mailrules/__init__.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 class UnresolvedLocalEmailAddressException(Exception):
     pass
